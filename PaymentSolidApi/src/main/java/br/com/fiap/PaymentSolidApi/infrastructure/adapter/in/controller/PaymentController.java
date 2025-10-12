ac48847396dd20a05eb4c7a85173a960fa3c87ee
Eduardo Abreu <edu.abreu09@hotmail.com>
2025-10-12 17:55:01 -0300

    Adiciona suporte a métricas do Prometheus e configura alertas no sistema de monitoramento

--- PaymentSolidApi/src/main/java/br/com/fiap/PaymentSolidApi/infrastructure/adapter/in/controller/PaymentController.java
+++ PaymentSolidApi/src/main/java/br/com/fiap/PaymentSolidApi/infrastructure/adapter/in/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package br.com.fiap.PaymentSolidApi.infrastructure.adapter.in.controller;
 
+import java.net.InetAddress;
 import java.net.URI;
 import java.util.UUID;
 
@@ -9,6 +10,7 @@ import br.com.fiap.paymentsolidiapi.api.PaymentsApi;
 import org.openapitools.model.PaymentRequestDTO;
 import org.openapitools.model.PaymentResponseDTO;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,34 +20,50 @@ public class PaymentController implements PaymentsApi {
 
     private final PaymentService paymentService;
 
+    // Injeta o valor da variável de ambiente 'INSTANCE_ID'.
+    // Se não for definida, usa 'instancia-padrao'.
+    @Value("${INSTANCE_ID:instancia-padrao}")
+    private String instanceId;
+    
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @Override
     public ResponseEntity<PaymentResponseDTO> createPayment(@RequestBody PaymentRequestDTO request) {
+        System.out.println("LOG: Requisição 'createPayment' recebida pela instância: " + instanceId);
+
         final var createdPayment = paymentService.create(PaymentMapper.fromRequestDto(request));
         final var uri = URI.create("/api/payments/" + createdPayment.getId());
 
         return ResponseEntity.created(uri).body(PaymentMapper.toResponseDto(createdPayment));
     }
-    
+
     @Override
     public ResponseEntity<PaymentResponseDTO> findPaymentById(@PathVariable("id") UUID id) {
+        System.out.println("LOG: Requisição 'findPaymentById' para o ID " + id + " recebida pela instância: " + instanceId);
+
         var paymentOpt = paymentService.findById(id);
         return paymentOpt.map(payment ->
                 ResponseEntity.ok(PaymentMapper.toResponseDto(payment))).orElseGet(() ->
                 ResponseEntity.noContent().build());
     }
 
-//    @Override
-//    public ResponseEntity<ReceiptResponseDTO> findReceiptById(@PathVariable("id") UUID id) {
-//        return null;
-//    }
-
     @Override
     public ResponseEntity<PaymentResponseDTO> refundPayment(@PathVariable("id") UUID id) {
+        System.out.println("LOG: Requisição 'refundPayment' para o ID " + id + " recebida pela instância: " + instanceId);
+
         final var paymentRefunded = paymentService.refundPayment(id);
         return ResponseEntity.ok().body(PaymentMapper.toResponseDto(paymentRefunded));
+    }
+
+    @GetMapping("/payments/host")
+    public ResponseEntity<String> getHost() {
+        try {
+            String hostname = InetAddress.getLocalHost().getHostName();
+            return ResponseEntity.ok("Requisição processada pela instância do container (hostname): " + hostname);
+        } catch (Exception e) {
+            return ResponseEntity.ok("Não foi possível obter o hostname da instância.");
+        }
     }
 }
